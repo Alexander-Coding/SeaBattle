0a06b96287085afd4550c4f77b3af7c796554b47
Alexander-Coding <71774264+Alexander-Coding@users.noreply.github.com>
2025-11-05 11:28:58 +0700

    Добавлен класс конфигурации проекта

---
+++ src/config.py
@@ -0,0 +1,35 @@
+from pydantic_settings import BaseSettings
+
+
+class Config(BaseSettings):
+    # Database
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+    DB_HOST: str
+    DB_PORT: int
+
+    # Security
+    SECRET_KEY: str
+    ALGORITHM:  str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+
+    @property
+    def database_url(self) -> str:
+        return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+
+    @property
+    def sync_database_url(self) -> str:
+        return f"postgresql://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+
+    class Config:
+        env_file = ".env"
+        case_sensitive = True
+
+
+config = Config()
+
+
+__all__ = [
+    'config'
+]
